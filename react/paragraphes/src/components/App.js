3582421b443e488281b6bae9fffec1fd758d4cf3
Yvan Douënel <y.douenel@coopernet.fr>
2022-12-12 13:03:50 +0100

    Exo paragraphes en cours.

--- react/paragraphes/src/components/App.js
+++ react/paragraphes/src/components/App.js
@@ -63,7 +63,7 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <h1 className="mt-4">Exercice des paragraphes</h1>
+        <h1 className="mt-4">Exercice des paragraphes {this.props.toto}</h1>
         <FormAddSection handleSubmitAddSection={this.handleSubmitAddSection} />
 
         {/* Affichage du formulaire de modification d'une section */}
