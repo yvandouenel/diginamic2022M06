993cc20ba487174d083b1bce514b49894d503d6b
Yvan Douënel <y.douenel@coopernet.fr>
2022-12-12 16:47:51 +0100

    Mode strict remis dans l'applic paragraphes.

--- react/paragraphes/src/index.js
+++ react/paragraphes/src/index.js
@@ -5,7 +5,11 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App toto="titi" />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
